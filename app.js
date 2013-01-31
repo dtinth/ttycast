77356fece92300268bbf19e9efc08ae0fe790e75
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-02-01 01:36:14 +0700

    run a headless xterm emulator inside the server (based on tty.js)

--- app.js
+++ app.js
@@ -5,35 +5,38 @@ var connect = require('connect')
   , server = require('http').createServer(app)
   , path = require('path')
   , send = require('send')
-  , termPath = require.resolve('tty.js/static/term')
+  , HeadlessTerminal = require('./lib/headless-terminal')
+  , term = new HeadlessTerminal(80, 25)
+  , bufferPath = require.resolve('./lib/screen-buffer')
 
 var io = require('socket.io').listen(server)
 io.set('log level', 2)
-var past = ''
+
 app.use(connect.static(__dirname + '/static'))
-app.use('/term', function(req, res, next) {
-  send(req, '/' + path.basename(termPath)).root(path.dirname(termPath))
+
+app.use('/screen-buffer.js', function(req, res, next) {
+  send(req, '/' + path.basename(bufferPath)).root(path.dirname(bufferPath))
     .pipe(res)
 })
 
-//var play = spawn('ttyplay', [ '-p', process.argv[2] ])
 process.stdin.resume()
+
 process.stdin.on('data', function(buf) {
   var str = buf.toString('utf8')
-  past += str
-  var idx = past.lastIndexOf('\x1b\x5b\x48\x1b\x5b\x32\x4a')
-  if (~idx) {
-    past = past.substr(idx)
-  }
-  io.sockets.emit('data', str)
-  console.log('past size: ' + past.length)
+  try { term.write(str) } catch (e) { }
 })
+
+term.on('screen', function(operations) {
+  io.sockets.emit('data', operations)
+})
+
 process.stdin.on('end', function() {
   console.log('died')
   process.exit(1)
 })
+
 io.sockets.on('connection', function(sock) {
-  sock.emit('data', past)
+  sock.emit('data', term.getInitialOperations())
 })
 
 server.listen(Number(process.env.PORT) || 13377)
