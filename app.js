d3bb7a922b82d5c87a341a6b65eda7ead4e501f7
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-02-01 13:25:59 +0700

    refactor the patcher out of the headless terminal.

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 #!/usr/bin/env node
 
-// require the libraries
+// parse the command line
+var program = require('commander')
+
+program
+  .option('-r, --rows', 'Number of rows in the broadcasting terminal')
+  .option('-c, --cols', 'Number of columns in the broadcasting terminal')
+  .parse(process.argv)
+
+var rows = program.rows || 80
+  , cols = program.cols || 25
+
+
+// create the server and require other libraries
 var connect = require('connect')
   , app = connect.createServer()
   , server = require('http').createServer(app)
   , path = require('path')
   , send = require('send')
+  , ScreenBuffer = require('./lib/screen-buffer')
+  , patcher = require('./lib/buffer-patcher')
+
 
 // create socket.io server
 var io = require('socket.io').listen(server)
 io.set('log level', 2)
+
 
 // serve static files
 app.use(connect.static(__dirname + '/static'))
@@ -20,6 +36,7 @@ app.use('/screen-buffer.js', function(req, res, next) {
   send(req, '/' + path.basename(bufferPath)).root(path.dirname(bufferPath))
     .pipe(res)
 })
+
 
 // create a terminal emulator
 var HeadlessTerminal = require('./lib/headless-terminal')
@@ -37,15 +54,29 @@ process.stdin.on('end', function() {
   process.exit(1)
 })
 
-// broadcast the changes to the screen buffer to all clients
-term.on('screen', function(operations) {
-  io.sockets.emit('data', operations)
+
+// the display as seen by clients
+var buffer = new ScreenBuffer()
+
+// when a client is connected, it is initialized with an empty buffer.
+// we patch its buffer to our current state
+io.sockets.on('connection', function(sock) {
+  var client = new ScreenBuffer()
+  io.sockets.emit('data', patcher.patch(client, buffer))
 })
 
-// send the initial data to newly-connected client
-io.sockets.on('connection', function(sock) {
-  sock.emit('data', term.getInitialOperations())
+// when the terminal's screen buffer is changed,
+// we patch our buffer to match with the terminal's buffer,
+// and broadcast the patch
+var timeout = null
+term.on('change', function() {
+  if (timeout == null) timeout = setTimeout(broadcast, 1000 / 30)
 })
+function broadcast() {
+  timeout = null
+  io.sockets.emit('data', patcher.patch(buffer, term.displayBuffer))
+}
+
 
 // listen
 server.listen(Number(process.env.PORT) || 13377)
