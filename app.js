734a714fb54e7f0a904434ae215240176b2c735e
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-02-01 12:50:48 +0700

    add comments to app.js

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
 #!/usr/bin/env node
 
+// require the libraries
 var connect = require('connect')
   , app = connect.createServer()
   , server = require('http').createServer(app)
   , path = require('path')
   , send = require('send')
-  , HeadlessTerminal = require('./lib/headless-terminal')
-  , term = new HeadlessTerminal(80, 25)
-  , bufferPath = require.resolve('./lib/screen-buffer')
 
+// create socket.io server
 var io = require('socket.io').listen(server)
 io.set('log level', 2)
 
+// serve static files
 app.use(connect.static(__dirname + '/static'))
 
+// serve the screen buffer library
+var bufferPath = require.resolve('./lib/screen-buffer')
 app.use('/screen-buffer.js', function(req, res, next) {
   send(req, '/' + path.basename(bufferPath)).root(path.dirname(bufferPath))
     .pipe(res)
 })
 
-process.stdin.resume()
+// create a terminal emulator
+var HeadlessTerminal = require('./lib/headless-terminal')
+  , term = new HeadlessTerminal(80, 25)
 
+// pipe the data to this terminal
 term.open()
-
+process.stdin.resume()
 process.stdin.on('data', function(buf) {
   var str = buf.toString('utf8')
   try { term.write(str) } catch (e) { console.log(e); console.log(e.stack) }
 })
-
-term.on('screen', function(operations) {
-  io.sockets.emit('data', operations)
-})
-
 process.stdin.on('end', function() {
   console.log('died')
   process.exit(1)
 })
 
+// broadcast the changes to the screen buffer to all clients
+term.on('screen', function(operations) {
+  io.sockets.emit('data', operations)
+})
+
+// send the initial data to newly-connected client
 io.sockets.on('connection', function(sock) {
   sock.emit('data', term.getInitialOperations())
 })
 
+// listen
 server.listen(Number(process.env.PORT) || 13377)
 
