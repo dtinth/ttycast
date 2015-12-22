08d8fd9b4de6743bc939053e0e1397a5a8b88ffe
Stephen Blott <smblott+github@gmail.com>
2015-12-22 15:14:16 +0000

    Rename 'data' as 'terminal-data'.
    If you want to use the socket connection for transporting anything other
    than terminal data, then the name 'data' is probably too generic.  If we
    make it more descriptive, then the sources and destinations are clearer.
    
    Use case:  I want to have a terminal window editing a file, but also the
    full file contents below.  And, to transmit those contents, I want to
    re-use the cocket connection.  I'd like the naming of the terminal data
    to be more descriptive.

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ var buffer = new ScreenBuffer()
 // when a client is connected, it is initialized with an empty buffer.
 // we patch its buffer to our current state
 io.sockets.on('connection', function(sock) {
-  io.sockets.emit('data', ScreenBuffer.diff(new ScreenBuffer(), buffer))
+  io.sockets.emit('terminal-data', ScreenBuffer.diff(new ScreenBuffer(), buffer))
 })
 
 // when the terminal's screen buffer is changed,
@@ -89,7 +89,7 @@ function broadcast() {
   var operations = ScreenBuffer.diff(buffer, term.displayBuffer)
   if (operations.length === 0) return
 
-  io.sockets.emit('data', operations)
+  io.sockets.emit('terminal-data', operations)
   ScreenBuffer.patch(buffer, operations)
 }
 
